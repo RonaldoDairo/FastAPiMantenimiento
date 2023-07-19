9c3db34f9dafbab994cb01bb5de6f9c052918e1f
Here <programaciondairo@gmail.com>
2023-07-19 23:39:06 +0200

    fastApi

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+app = FastAPI()
+
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: Optional[str]
+
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+@app.get("/libro/{id}")
+def mostrar_libros(id: int):
+    return {"data": id}
+
+@app.post("/libros")
+def insert_libro(libro: Libro):
+    return {"message": f"Libro {libro.titulo} insertado"}
